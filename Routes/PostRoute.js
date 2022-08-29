25abaa215636782195a262bfa0f95d90e9586e25
Rizwan Ahmed <rizwanahmed060602@gmail.com>
2022-08-29 19:32:19 +0500

    Backend Completed

--- Routes/PostRoute.js
+++ Routes/PostRoute.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import {createPost, getPost, updatePost} from '../Controllers/PostController.js'
+import {createPost, getPost, updatePost, deletePost, likePost, getTimelinePosts} from '../Controllers/PostController.js'
 
 const router = express.Router();
 
 router.post('/', createPost)
 router.get('/:id', getPost)
 router.put('/:id', updatePost)
+router.delete('/:id', deletePost)
+router.post('/:id/like', likePost)
+router.get("/:id/timeline", getTimelinePosts)
 
 export default router
