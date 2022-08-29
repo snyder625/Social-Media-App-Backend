4cc87c45ad166b90850df08c878d777d33e0d297
Rizwan Ahmed <rizwanahmed060602@gmail.com>
2022-08-29 11:49:07 +0500

    Environment File recreated

--- Controllers/PostController.js
+++ Controllers/PostController.js
@@ -1,14 +1,45 @@
-import postModel from '../Models/postModel.js'
+import PostModel from '../Models/postModel.js'
 import mongoose from 'mongoose';
 
 //Create new Post
 export const createPost = async (req, res) => {
-    const newPost = new postModel(req.body);
+    const newPost = new PostModel(req.body);
 
     try {
         await newPost.save();
         res.status(200).json("Post Created");
     } catch (error) {
         res.status(500).json(error)
+    }
+};
+
+//Get a Post
+export const getPost = async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        const post = await PostModel.findById(id);
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+};
+
+//Update a Post
+export const updatePost = async (req, res) => {
+    const postId = req.params.id;
+    const {userId} = req.body;
+
+    try {
+        const post = await PostModel.findById(postId);
+        if(post.userId === userId) {
+            await post.updateOne({$set: req.body});
+            res.status(200).json("Post Updated");
+        }
+        else {
+            res.status(403).json("Access Forbidden");
+        }
+    } catch (error) {
+        res.status(500).json(error);
     }
 }
--- Routes/PostRoute.js
+++ Routes/PostRoute.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import {createPost} from '../Controllers/PostController.js'
+import {createPost, getPost, updatePost} from '../Controllers/PostController.js'
 
 const router = express.Router();
 
 router.post('/', createPost)
+router.get('/:id', getPost)
+router.put('/:id', updatePost)
 
 export default router
